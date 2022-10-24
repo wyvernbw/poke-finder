76bb3f0570efd8dc3364d028f34758bce6faaaf4
wyvernbw <iamwyvern511@gmail.com>
2022-10-24 23:44:26 +0300

    trim fat

--- src/server/trpc/router/_app.ts
+++ src/server/trpc/router/_app.ts
@@ -1,13 +1,9 @@
 // src/server/router/_app.ts
 import { router } from "../trpc";
 
-import { exampleRouter } from "./example";
 import { pokemonRouter } from "./pokemon";
-import { testRouter } from "./test";
 
 export const appRouter = router({
-  example: exampleRouter,
-  test: testRouter,
   pokemon: pokemonRouter,
 });
 
